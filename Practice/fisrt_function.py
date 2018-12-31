085b2f6897bc220350faa9ba6839057f19d4356c
nishutam94 <nishu.tam@gmail.com>
2018-12-31 12:34:12 +0530

    Added the Practicle directory

---
+++ Practice/fisrt_function.py
@@ -0,0 +1,11 @@
+def main():
+    print("main function")
+    print(first_function())
+
+def first_function():
+    print("This is first function made by me")
+
+first_function()
+
+if __name__ == "__main__":
+    main()
