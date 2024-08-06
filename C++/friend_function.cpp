7bcb2aee2046eb6bd99ec3e7a5ce7a55815f1e8c
Nishant Tamrakar <nishant.tamrakar@intel.com>
2024-08-06 05:02:48 -0400

    update the code

--- C++/friend_function.cpp
+++ C++/friend_function.cpp
@@ -1,42 +1,13 @@
 #include<iostream>
 #include<stdio.h>
 #include<fstream>
+using namespace std;
 
-struct syst;
-struct syst
-{
-};
 
-template <typename T> 
+// with the help of friend class and friend function we can accessprivate and protected the member of 
+// other class. 
 
-T mymax(T x, T y)
-{
-    return (x>y)?x:y;
-}
 
-class base
-{   public:
-    virtual void function()
-    {
-        std::cout<<"this is base function call\n";
-    }
-    void function2()
-    {
-        std::cout<<"this is base function2 call\n";
-    }
-};
-class drived:public base
-{   
-    public:
-    void function()
-    {
-        std::cout<<"this is drived function call\n";
-    }
-    void function2()
-    {
-        std::cout<<"this is drived function2 call\n";
-    }
-};
 class Geeks 
 {   
     private:
@@ -70,51 +41,11 @@ void Geeks :: printnameoutsideclass()
 }
 int main(void)
 {   
-    using namespace std;
-    cout << "Hello,world\n";
-    printf("%s","hello,World\n");
     Geeks G1;
     G1.GeeksName="Abhi";
     G1.printname();
     G1.printnameoutsideclass();
     frindclass Fc1;
     Fc1.friendclassfun(G1);
-    base * baseptr1 = new base();
-    baseptr1->function();
-    baseptr1->function2();
-    base * baseptr2 = new drived();//base calss pointer is accesing the 
-    baseptr2->function();
-    baseptr2->function2();
-    
-    
-    string line;
-    fstream fout;
-    fout.open("sample.txt");
-    if (!fout.is_open()) {
-        cerr << "Error opening file" << endl;
-        return 1;
-    }
-    while (fout)
-    {
-        getline(cin,line);
-        if (line == "-1")
-        {
-            break;
-        }
-        fout << line << endl;
-    }
-    fout.close();
-
-    ifstream fin;
-    fin.open("sample.txt");
-
-    while (getline(fin, line)) 
-    {
-        cout << line << endl;
-    }
- 
-    fin.close();
-
-    cout << mymax<int>(3,4)<<endl;
     return 0;
 }
