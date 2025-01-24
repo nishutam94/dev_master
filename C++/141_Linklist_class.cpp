af3465f96613fccd82ed320a720afc373c6561f8
root <root@emr-bkc.iind.intel.com>
2025-01-24 04:01:53 -0500

    add

---
+++ C++/141_Linklist_class.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data ;
+    Node * next;
+    Node(int x)
+    {
+        data =data;
+        next = NULL;
+    }
+};
+
+
+int main()
+{
+    Node * newnode = new Node(10);
+    pair<int,int> p1;
+    p1={1,2};
+
+    cout<<p1.first;
+    return 0;
+
+}
